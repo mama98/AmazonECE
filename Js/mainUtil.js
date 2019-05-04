be20f4fe61bbb7ee17b6a931f4044af0dd9a026d
yasminetahri <36414394+yasminetahri@users.noreply.github.com>
2019-05-04 10:00:29 +0200

    Add files via upload

---
+++ Js/mainUtil.js
@@ -0,0 +1,12 @@
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(()=>{
+    // Login Register Form
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
